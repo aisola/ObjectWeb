3785432e1ac9c926a987242246e3b538f28e0bbb
Abram C. Isola <abram@isola.mn>
2013-05-10 00:12:45 -0500

    Added the Python Distutils Setup file.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python 
+
+from distutils.core import setup
+
+setup(
+    name='ObjectWeb',
+    version='1.0 (Beta)',
+    author='Abram C. Isola',
+    author_email='abram@isola.mn',
+    packages=['ObjectWeb'],
+    url='http://abram.isola.mn/project/ObjectWeb',
+    description='A pure-Python standalone Web Framework for WSGI and CGI.',
+)
