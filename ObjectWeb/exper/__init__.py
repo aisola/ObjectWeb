fecb9b68789508e512c0924ca3bdd09fc17f914b
Abram Isola <abram@isola.mn>
2014-01-02 10:20:27 -0600

    clean up git deletions

--- ObjectWeb/exper/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-################################################################################
-## contact: abram@isola.mn || https://github.com/aisola/ObjectWeb
-## license: LGPLv3
-## summary: This document makes the 'exper' module.
-## maintainer: Abram C. Isola <abram@isola.mn>
-## contrib: Abram C. Isola <abram@isola.mn> (all)
-################################################################################
