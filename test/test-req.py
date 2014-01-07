f450d8557ca75c3bbb52340ede3878d40bec3bba
Abram C. Isola <abram@isola.mn>
2014-01-07 11:38:01 -0600

    Update .travis.yml and try to make test scripts.

---
+++ test/test-req.py
@@ -0,0 +1,46 @@
+
+import sys
+import requests
+
+NO_ERROR = True
+ERRORS = []
+
+# Standard Test
+res = requests.get("http://localhost:8080/")
+if not res.status_code == 200:
+    NO_ERROR = False
+    ERRORS.append("standard")
+
+# Redirect Test
+res = requests.get("http://localhost:8080/redirect",allow_redirects=False)
+if not res.status_code == 303:
+    NO_ERROR = False
+    ERRORS.append("redirect")
+
+# Environment Test
+res = requests.get("http://localhost:8080/env")
+if not res.headers.get("Content-Type") == "text/plain;charset=utf-8":
+    NO_ERROR = False
+    ERRORS.append("env-content")
+if not res.headers.get("X-Powered-By") == "ObjectWeb/2.0":
+    NO_ERROR = False
+    ERRORS.append("env-powered")
+
+# Cookie Handling Test
+res = requests.get("http://localhost:8080/cookie")
+if not res.cookies.get("CookieName", "") == "CookieValue":
+    NO_ERROR = False
+    ERRORS.append("cookie-get")
+
+res = requests.post("http://localhost:8080/cookie", cookies={"CookieName":"CookieValue"})
+if not res.status_code == 200:
+    NO_ERROR = False
+    ERRORS.append("cookie-return")
+
+
+if NO_ERROR == True:
+    print 0, ", ".join(ERRORS)
+    sys.exit(0)
+else:
+    print 1, ", ".join(ERRORS)
+    sys.exit(1)
---
+++ test/test-server.py
@@ -0,0 +1,52 @@
+
+import sys
+
+sys.path.insert(0, '..')
+
+import ObjectWeb
+
+class MainPage(object):
+    
+    def GET(self):
+        ObjectWeb.status("200 OK")
+        return "PASS"
+
+    def POST(self):
+        ObjectWeb.status("200 OK")
+        return "REDIRECT PASS"
+
+class RedirectHandler(object):
+    
+    def GET(self):
+        ObjectWeb.seeother("/")
+
+class EnvHandler(object):
+    
+    def GET(self):
+        ObjectWeb.header("Content-Type", "text/plain")
+        ObjectWeb.header("X-Powered-By", "ObjectWeb/2.0")
+        return str(ObjectWeb.context)
+
+class CookieRelay(object):
+    
+    def GET(self):
+        ObjectWeb.status("200 OK")
+        ObjectWeb.setcookie("CookieName","CookieValue")
+        return ""
+    
+    def POST(self):
+        cookies = ObjectWeb.cookies()
+        if not cookies:
+            ObjectWeb.status("404 Not Found")
+            return ""
+        else:
+            ObjectWeb.status("200 OK")
+            return cookies.get("CookieName","")
+        
+
+app = ObjectWeb.Application({
+    "/": MainPage,
+    "/redirect": RedirectHandler,
+    "/env": EnvHandler,
+    "/cookie": CookieRelay,
+}).run(port=8080)
