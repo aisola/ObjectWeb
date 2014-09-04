e12058e53a537f59939b6a4f06a95b970f71b790
Abram C. Isola <abram@ave81.com>
2014-09-04 18:54:10 -0500

    further sort debug.py

--- ObjectWeb/debug.py
+++ ObjectWeb/debug.py
@@ -5,6 +5,38 @@ from cgi import escape
 
 import webapi
 
+page_css = """html * {padding:0; margin:0;} body * {padding:10px 20px;} body * * {padding:0;} body {font:small sans-serif;}
+body>div {border-bottom:1px solid #ddd;} h1 {font-weight:normal;} h2 {margin-bottom:.8em;} h2 span {font-size:80%;
+color:#666; font-weight:normal;} h3 { margin:1em 0 .5em 0; } h4 {margin:0 0 .5em 0; font-weight: normal;} table {border:1px solid #ccc;
+border-collapse: collapse; background:white;} tbody td, tbody th {vertical-align:top; padding:2px 3px;} thead th {padding:1px 6px 1px 3px;
+background:#fefefe; text-align:left; font-weight:normal; font-size:11px; border:1px solid #ddd;} tbody th { text-align:right;
+color:#666; padding-right:.5em;} table.vars {margin:5px 0 2px 40px;} table.vars td, table.req td {font-family:monospace;}
+table td.code {width:100%;} table td.code div {overflow:hidden;} table.source th {color:#666;} table.source td {font-family:monospace;
+white-space:pre; border-bottom:1px solid #eee;} ul.traceback {list-style-type:none;} ul.traceback li.frame {margin-bottom:1em;}
+div.context {margin: 10px 0;} div.context ol {padding-left:30px; margin:0 10px; list-style-position: inside;}
+div.context ol li {font-family:monospace; white-space:pre; color:#666; cursor:pointer;} div.context ol.context-line li {color:black;
+background-color:#ccc;} div.context ol.context-line li span {float: right;} div.commands {margin-left: 40px;}
+div.commands a {color:black; text-decoration:none;} #summary {background: #ffc;} #summary h2 {font-weight: normal;
+color: #666;} #explanation {background:#eee;} #template, #template-not-exist {background:#f6f6f6;}
+#template-not-exist ul {margin: 0 0 0 20px;} #traceback {background:#eee;} #requestinfo {background:#f6f6f6;
+padding-left:120px;} #summary table {border:none; background:transparent;} #requestinfo h2, #requestinfo h3 {position:relative;
+margin-left:-100px;} #requestinfo h3 {margin-bottom:-1em;} .error {background: #ffc;} .specific {color:#cc3300; font-weight:bold;}
+"""
+
+page_js = """
+function getElementsByClassName(oElm, strTagName, strClassName){var arrElements = (strTagName == "*" && document.all)? document.all :
+oElm.getElementsByTagName(strTagName);var arrReturnElements = new Array();strClassName = strClassName.replace(/\-/g, "\\-");
+var oRegExp = new RegExp("(^|\\s)" + strClassName + "(\\s|$$)");var oElement;for(var i=0; i<arrElements.length; i++){
+oElement = arrElements[i];if(oRegExp.test(oElement.className)){arrReturnElements.push(oElement);}}return (arrReturnElements)}
+function hideAll(elems) {for (var e = 0; e < elems.length; e++) {elems[e].style.display = 'none';}}
+window.onload = function() {hideAll(getElementsByClassName(document, 'table', 'vars'));
+hideAll(getElementsByClassName(document, 'ol', 'pre-context'));hideAll(getElementsByClassName(document, 'ol', 'post-context'));}
+function toggle() {for (var i = 0; i < arguments.length; i++) {var e = document.getElementById(arguments[i]);
+if (e) {e.style.display = e.style.display == 'none' ? 'block' : 'none';}} return false;}
+function varToggle(link, id) {toggle('v' + id);var s = link.getElementsByTagName('span')[0];var uarr = String.fromCharCode(0x25b6);
+var darr = String.fromCharCode(0x25bc);s.innerHTML = s.innerHTML == uarr ? darr : uarr;return false;}
+"""
+
 error_template_start = """
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html lang="en">
@@ -13,104 +45,15 @@ error_template_start = """
   <meta name="robots" content="NONE,NOARCHIVE" />
   <title>ObjectWeb Internal Error Debugging</title>
   <style type="text/css">
-    html * { padding:0; margin:0; }
-    body * { padding:10px 20px; }
-    body * * { padding:0; }
-    body { font:small sans-serif; }
-    body>div { border-bottom:1px solid #ddd; }
-    h1 { font-weight:normal; }
-    h2 { margin-bottom:.8em; }
-    h2 span { font-size:80%; color:#666; font-weight:normal; }
-    h3 { margin:1em 0 .5em 0; }
-    h4 { margin:0 0 .5em 0; font-weight: normal; }
-    table { 
-        border:1px solid #ccc; border-collapse: collapse; background:white; }
-    tbody td, tbody th { vertical-align:top; padding:2px 3px; }
-    thead th { 
-        padding:1px 6px 1px 3px; background:#fefefe; text-align:left; 
-        font-weight:normal; font-size:11px; border:1px solid #ddd; }
-    tbody th { text-align:right; color:#666; padding-right:.5em; }
-    table.vars { margin:5px 0 2px 40px; }
-    table.vars td, table.req td { font-family:monospace; }
-    table td.code { width:100%;}
-    table td.code div { overflow:hidden; }
-    table.source th { color:#666; }
-    table.source td { 
-        font-family:monospace; white-space:pre; border-bottom:1px solid #eee; }
-    ul.traceback { list-style-type:none; }
-    ul.traceback li.frame { margin-bottom:1em; }
-    div.context { margin: 10px 0; }
-    div.context ol { 
-        padding-left:30px; margin:0 10px; list-style-position: inside; }
-    div.context ol li { 
-        font-family:monospace; white-space:pre; color:#666; cursor:pointer; }
-    div.context ol.context-line li { color:black; background-color:#ccc; }
-    div.context ol.context-line li span { float: right; }
-    div.commands { margin-left: 40px; }
-    div.commands a { color:black; text-decoration:none; }
-    #summary { background: #ffc; }
-    #summary h2 { font-weight: normal; color: #666; }
-    #explanation { background:#eee; }
-    #template, #template-not-exist { background:#f6f6f6; }
-    #template-not-exist ul { margin: 0 0 0 20px; }
-    #traceback { background:#eee; }
-    #requestinfo { background:#f6f6f6; padding-left:120px; }
-    #summary table { border:none; background:transparent; }
-    #requestinfo h2, #requestinfo h3 { position:relative; margin-left:-100px; }
-    #requestinfo h3 { margin-bottom:-1em; }
-    .error { background: #ffc; }
-    .specific { color:#cc3300; font-weight:bold; }
+        %s
   </style>
   <script type="text/javascript">
   //<!--
-    function getElementsByClassName(oElm, strTagName, strClassName){
-        // Written by Jonathan Snook, http://www.snook.ca/jon; 
-        // Add-ons by Robert Nyman, http://www.robertnyman.com
-        var arrElements = (strTagName == "*" && document.all)? document.all :
-        oElm.getElementsByTagName(strTagName);
-        var arrReturnElements = new Array();
-        strClassName = strClassName.replace(/\-/g, "\\-");
-        var oRegExp = new RegExp("(^|\\s)" + strClassName + "(\\s|$$)");
-        var oElement;
-        for(var i=0; i<arrElements.length; i++){
-            oElement = arrElements[i];
-            if(oRegExp.test(oElement.className)){
-                arrReturnElements.push(oElement);
-            }
-        }
-        return (arrReturnElements)
-    }
-    function hideAll(elems) {
-      for (var e = 0; e < elems.length; e++) {
-        elems[e].style.display = 'none';
-      }
-    }
-    window.onload = function() {
-      hideAll(getElementsByClassName(document, 'table', 'vars'));
-      hideAll(getElementsByClassName(document, 'ol', 'pre-context'));
-      hideAll(getElementsByClassName(document, 'ol', 'post-context'));
-    }
-    function toggle() {
-      for (var i = 0; i < arguments.length; i++) {
-        var e = document.getElementById(arguments[i]);
-        if (e) {
-          e.style.display = e.style.display == 'none' ? 'block' : 'none';
-        }
-      }
-      return false;
-    }
-    function varToggle(link, id) {
-      toggle('v' + id);
-      var s = link.getElementsByTagName('span')[0];
-      var uarr = String.fromCharCode(0x25b6);
-      var darr = String.fromCharCode(0x25bc);
-      s.innerHTML = s.innerHTML == uarr ? darr : uarr;
-      return false;
-    }
+        %s
     //-->
   </script>
 </head>
-<body>"""
+<body>""" % (page_css, page_js)
 
 error_template_end = """
 <div id="explanation">
@@ -136,11 +79,9 @@ def _get_lines_from_file(filename, lineno, context_lines):
         lower_bound = max(0, lineno - context_lines)
         upper_bound = lineno + context_lines
 
-        pre_context = \
-            [line.strip('\n') for line in source[lower_bound:lineno]]
+        pre_context = [line.strip('\n') for line in source[lower_bound:lineno]]
         context_line = source[lineno].strip('\n')
-        post_context = \
-            [line.strip('\n') for line in source[lineno + 1:upper_bound]]
+        post_context = [line.strip('\n') for line in source[lineno + 1:upper_bound]]
 
         return lower_bound, pre_context, context_line, post_context
     except (OSError, IOError, IndexError):
